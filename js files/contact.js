b6bb8b4d78f1c03c50faffb72de790d92189712c
Ben Dhiman <mac@Macs-MacBook.local>
2022-05-31 20:57:57 +0200

    Lots of fun and interesting changes

--- js files/contact.js
+++ js files/contact.js
@@ -39,34 +39,34 @@ $(window).on("load", function(){
 }, timeDelay)
 });
 
-const clusterPics = gsap.utils.toArray(".cluster-pic");
+// const clusterPics = gsap.utils.toArray(".cluster-pic");
 
 
-clusterPics.forEach(pic => {
-    gsap.from(pic, {
-        scale: 0.2,
-        autoAlpha: 0,
-        duration: 2,
-        x: 500,
-        stagger: 0.3,
-        ease: "power2.in"
-    })
-});
+// clusterPics.forEach(pic => {
+//     gsap.from(pic, {
+//         scale: 0.2,
+//         autoAlpha: 0,
+//         duration: 2,
+//         x: 500,
+//         stagger: 0.3,
+//         ease: "power2.in"
+//     })
+// });
 
-const spinButton = document.querySelector(".animate-link");
+// const spinButton = document.querySelector(".animate-link");
 
-spinButton.addEventListener('mouseenter', () => {
-    gsap.to("#star-spin", {
-        duration: 0.5,
-        rotate: 360,
-        ease: 'power2.inOut'
-    })
-})
-spinButton.addEventListener('mouseleave', () => {
-    gsap.set("#star-spin", {
-        duration: 0.5,
-        rotate: 0,
-        ease: 'power2.inOut',
+// spinButton.addEventListener('mouseenter', () => {
+//     gsap.to("#star-spin", {
+//         duration: 0.5,
+//         rotate: 360,
+//         ease: 'power2.inOut'
+//     })
+// })
+// spinButton.addEventListener('mouseleave', () => {
+//     gsap.set("#star-spin", {
+//         duration: 0.5,
+//         rotate: 0,
+//         ease: 'power2.inOut',
         
-    })
-});
+//     })
+// });
