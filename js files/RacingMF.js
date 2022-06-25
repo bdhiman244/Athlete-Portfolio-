1c0a67e7aaa6b879e848150e04489ce7d70b4d66
Ben Dhiman <mac@Macs-MacBook.local>
2022-06-25 14:44:27 +0200

    revamped racing page, fully responsive

---
+++ js files/RacingMF.js
@@ -0,0 +1,26 @@
+const navBtn = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav");
+const circleSVG = document.querySelector(".header-svg");
+
+
+navBtn.addEventListener('click', () => {
+navMenu.classList.toggle("nav--visibile");
+});
+
+gsap.to(circleSVG, {
+    duration: 20,
+    rotate: 360,
+    repeat: -1,
+    ease: "none"
+});
+
+gsap.to(".bigorre-tiger", {
+    delay: 3,
+    duration: 4,
+    rotateX: -360,
+    repeat: -1,
+    ease: "slow"
+});
+
+
+
