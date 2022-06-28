6ebeea1d5c42f4d1a5b52daa11f1a1b478dd43a2
Ben Dhiman <mac@Macs-MacBook.local>
2022-06-28 22:49:44 +0200

    new contact page

---
+++ js files/contactMF.js
@@ -0,0 +1,14 @@
+const navBtn = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav");
+
+navBtn.addEventListener('click', () => {
+navMenu.classList.toggle("nav--visibile");
+});
+
+var tl = gsap.timeline();
+
+tl.from(" .title-text", {
+    duration: 1.5,
+    x: -300,
+    opacity: 0
+});
