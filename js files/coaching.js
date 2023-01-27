7c41bdb238e80f85a4f1a551fdef1fc835459400
Ben Dhiman <mac@Macs-MacBook.local>
2023-01-27 14:14:06 +0100

    new post

---
+++ js files/coaching.js
@@ -0,0 +1,7 @@
+const navBtn = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav");
+
+navBtn.addEventListener('click', () => {
+navMenu.classList.toggle("nav--visibile");
+});
+
