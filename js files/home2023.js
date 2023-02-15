c5be7897d54e51d3e5c02625d5602991ea33c137
Ben Dhiman <mac@Macs-MacBook.local>
2023-02-15 12:07:52 +0100

    new style sheet

---
+++ js files/home2023.js
@@ -0,0 +1,15 @@
+var tl = gsap.timeline();
+
+ gsap.from(".hero-img-container", {
+    duration: 2,
+    x: -700,
+    opacity: 0
+});
+
+gsap.from(".nav__item", {
+    delay: 2,
+    duration: 1.5,
+    opacity: 0,
+    x: -250,
+    stagger: 0.5
+}, "-=70%");
